e54b6dd197b89c787b4784ba2a9c3118132fdf53
Tue Ho <tuehd@citics.com.vn>
2022-07-11 16:52:26 +0700

    fix

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import { ApolloServer } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import express from "express";
-import http from "http";
-import mongoose from "mongoose";
-import { resolvers } from "./resolvers/index";
-import { typeDefs } from "./typeDefs/index";
-import Auth from "./services/auth.service";
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+import mongoose from 'mongoose';
+import { resolvers } from './resolvers/index';
+import { typeDefs } from './typeDefs/index';
+import Auth from './services/auth.service';
 // Provide resolver functions for your schema fields
 
 const connectDB = () => {
   try {
     mongoose
       .connect(
-        "mongodb+srv://tuehd:0917977835@cluster0.r5mdp.mongodb.net/?retryWrites=true&w=majority",
+        'mongodb+srv://tuehd:0917977835@cluster0.r5mdp.mongodb.net/?retryWrites=true&w=majority',
         {
           useNewUrlParser: true,
           useUnifiedTopology: true,
-        }
+        },
       )
       .then(() => {
-        console.log("oke db");
+        console.log('oke db');
       })
-      .catch((error) => console.log(error));
+      .catch(error => console.log(error));
   } catch (error) {
     console.log(error);
   }
@@ -37,7 +37,7 @@ async function startApolloServer(typeDefs, resolvers) {
     typeDefs,
     resolvers,
     csrfPrevention: true,
-    cache: "bounded",
+    cache: 'bounded',
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     context: ({ req }) => {
       return {
@@ -57,11 +57,13 @@ async function startApolloServer(typeDefs, resolvers) {
     // By default, apollo-server hosts its GraphQL endpoint at the
     // server root. However, *other* Apollo Server packages host it at
     // /graphql. Optionally provide this to match apollo-server.
-    path: "/",
+    path: '/',
   });
 
   // Modified server startup
-  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  await new Promise(resolve =>
+    httpServer.listen({ port: process.env.PORT || 4000 }, resolve),
+  );
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 }
 
